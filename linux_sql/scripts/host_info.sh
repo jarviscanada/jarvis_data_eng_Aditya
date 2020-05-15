90d7f81143492fbabc6e2a9f1f59869addf3a3ea
Aditya <adityasharm10@gmail.com>
2020-05-15 19:22:13 +0000

    host_info and host_usage complete with insert stmt

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -8,15 +8,18 @@ psql_password=$5
 hostname=$(hostname -f)
 lscpu_out=`lscpu`
 
+
 cpu_number=$(echo "$lscpu_out"  | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
 cpu_number=$(echo "$lscpu_out"  | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
 cpu_architecture=$(echo "$lscpu_out" | egrep "^ARCHITECTURE:" | awk '{print $2}' | xargs)
 cpu_model=$(echo "$lscpu_out" | egrep "^Model name:" | awk '{print $3, $4, $5, $6, $7}' | xargs)
 cpu_mhz=$(echo "$lscpu_out" | egrep "^CPU MHz:" | awk '{print $3}' | xargs)
-l2_cache=$(echo "$lscpu_out" | egrep "^L2 cache:" | awk '{print $3}' | xargs)
+l2_cache=$(echo "$lscpu_out" | egrep "^L2 cache:" | awk '{print $3}' | grep -o -E '[0-9]+' | awk '{print $1}' | xargs)
 total_mem=$(echo "$(cat /proc/meminfo)" | egrep "^MemTotal:" | awk '{print $2}' | xargs)
 timestamp=$(echo "$(vmstat -t)"  | egrep "^ 0 " | awk '{print $18, $19}' | xargs)
 
-insert_stmt="INSERT INTO host_info (hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, L2_cache, total_mem, "timestamp") VALUES ('${hostname}',${cpu_number},'${cpu_architecture}', '${cpu_model}', ${cpu_mhz}, ${l2_cache}, ${total_mem}, '${timestamp}')
 
-psql -h "$psql_host" -p $psql_port -U "$psql_user" -d "$db_name" -c $insert_stmt
+
+insert_stmt="INSERT INTO host_info (hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, L2_cache, total_mem, "timestamp") VALUES ('${hostname}',${cpu_number},'${cpu_architecture}', '${cpu_model}', ${cpu_mhz}, ${l2_cache}, ${total_mem}, '${timestamp}');"
+
+psql -h "$psql_host" -p "$psql_port" -U "$psql_user" -d "$db_name" -c "$insert_stmt"
--- linux_sql/scripts/host_usage.sh
+++ linux_sql/scripts/host_usage.sh
@@ -14,11 +14,10 @@ cpu_idle=$(echo "$(vmstat -t)"  | egrep "1" | awk '{print $15}' | xargs)
 cpu_kernel=$(echo "$(vmstat -t)"  | egrep "1" | awk '{print $14}' | xargs)
 disk_io=$(echo "$(vmstat -d)"  | egrep "1" | awk '{print $10}' | xargs)
 disk_available=$(echo "$(vmstat -t)"  | egrep "1" | awk '{print $4/1000}' | xargs)
-
-timestamp=$(echo "$(vmstat -t)"  | egrep "^ 0 " | awk '{print $18, $19}' | xargs)
-
+timestamp=$(echo "$(vmstat -t)"  | egrep "1" | awk '{print $18, $19}' | xargs)
 
 
-insert_stmt = INSERT INTO host_usage ("timestamp", host_id, memory_free, cpu_idle, cpu_kernel, disk_io, disk_avaiable) VALUES ('${timestamp}', SELECT MAX(id) FROM host_info;, ${memory_free}, ${cpu_idle} , ${cpu_kernel}, ${disk_io}, ${disk_avaiable} )
 
-psql -h "$psql_host" -p $psql_port -U "$psql_user" -d "$db_name" -c $insert_stmt
+insert_stmt="INSERT INTO host_usage ( "timestamp", host_id, memory_free, cpu_idle, cpu_kernel, disk_io, disk_available) VALUES ('${timestamp}', (SELECT id FROM host_info), ${memory_free}, ${cpu_idle} , ${cpu_kernel}, ${disk_io}, ${disk_available});"
+
+psql -h "$psql_host" -p $psql_port -U "$psql_user" -d "$db_name" -c "$insert_stmt"
