9b592c8c188c3d79eb20bc6bbdad4ba6b12e1094
Aditya <adityasharm10@gmail.com>
2020-05-17 20:01:39 +0000

    removed unnecessary new lines

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -8,7 +8,6 @@ psql_password=$5
 hostname=$(hostname -f)
 lscpu_out=`lscpu`
 
-
 cpu_number=$(echo "$lscpu_out"  | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
 cpu_number=$(echo "$lscpu_out"  | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
 cpu_architecture=$(echo "$lscpu_out" | egrep "^ARCHITECTURE:" | awk '{print $2}' | xargs)
@@ -17,11 +16,6 @@ cpu_mhz=$(echo "$lscpu_out" | egrep "^CPU MHz:" | awk '{print $3}' | xargs)
 l2_cache=$(echo "$lscpu_out" | egrep "^L2 cache:" | awk '{print $3}' | grep -o -E '[0-9]+' | awk '{print $1}' | xargs)
 total_mem=$(echo "$(cat /proc/meminfo)" | egrep "^MemTotal:" | awk '{print $2}' | xargs)
 timestamp=$(echo "$(vmstat -t)"  | egrep "1" | awk '{print $18, $19}' | xargs)
-
-
-
 insert_stmt="INSERT INTO host_info (hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, L2_cache, total_mem, "timestamp") VALUES ('${hostname}',${cpu_number},'${cpu_architecture}', '${cpu_model}', ${cpu_mhz}, ${l2_cache}, ${total_mem}, '${timestamp}');"
-
 psql -h "$psql_host" -p "$psql_port" -U "$psql_user" -d "$db_name" -c "$insert_stmt"
-
 exit $?
